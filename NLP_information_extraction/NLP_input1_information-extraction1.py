c4f7580798f79114c0c301940c52bc9513bbbf6c
Rahul <116004410+Rahuldahiya67@users.noreply.github.com>
2023-04-28 12:17:54 +0530

    Update NLP_input1_information-extraction1.py

--- NLP_information_extraction/NLP_input1_information-extraction1.py
+++ NLP_information_extraction/NLP_input1_information-extraction1.py
@@ -5,14 +5,14 @@ import spacy
 from spacy.cli import download
 import streamlit as st
 from pdfminer.high_level import extract_text
+import os
 
 # Download spaCy model if not installed
 if not spacy.util.is_package("en_core_web_sm"):
     download("en_core_web_sm")
 
-# Set spaCy model path explicitly
-spacy_model_path = "/path/to/en_core_web_sm" # Replace with the actual path where the model is installed on your system
-spacy.util.set_data_path(spacy_model_path)
+# Set spaCy model path using environment variable
+os.environ["SPACY_DATA"] = "/path/to/spacy/models" # Replace with the actual path where the models are stored on your system
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
@@ -31,6 +31,7 @@ if pdf_file is not None:
     # Display entities in Streamlit
     for ent in doc.ents:
         st.write(ent.text, ent.label_)
+
 
 
 
