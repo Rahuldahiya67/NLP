a9652db9e4629eedadd8789f9ed3a51d4d81f835
Rahul <116004410+Rahuldahiya67@users.noreply.github.com>
2023-04-28 11:55:58 +0530

    Update NLP_input1_information-extraction1.py

--- NLP_information_extraction/NLP_input1_information-extraction1.py
+++ NLP_information_extraction/NLP_input1_information-extraction1.py
@@ -1,54 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-# Import libraries
 import spacy
+from spacy.cli import download
 import streamlit as st
 from pdfminer.high_level import extract_text
+
+# Download spaCy model if not installed
+if not spacy.util.is_package("en_core_web_sm"):
+    download("en_core_web_sm")
+
+# Set spaCy model path explicitly
+spacy_model_path = "/path/to/en_core_web_sm" # Replace with the actual path where the model is installed on your system
+spacy.util.set_data_path(spacy_model_path)
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Define function for extracting entities
-def extract_entities(text):
+# Set PDF file path
+pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
+
+# If PDF file is uploaded
+if pdf_file is not None:
+    # Extract text from PDF file
+    text = extract_text(pdf_file)
+    
+    # Create a spaCy object from the text
     doc = nlp(text)
-    entities = []
+
+    # Display entities in Streamlit
     for ent in doc.ents:
-        entities.append((ent.text, ent.label_))
-    return entities
-
-# Define Streamlit app
-def main():
-    # Set app title
-    st.title("PDF Entity Recognition")
-
-    # Add file uploader
-    st.sidebar.title("Upload PDF")
-    uploaded_file = st.sidebar.file_uploader("", type="pdf")
-
-    # Process uploaded file
-    if uploaded_file is not None:
-        # Extract text from PDF
-        text = extract_text(uploaded_file)
-        # Display text
-        st.subheader("PDF Text:")
-        st.write(text)
-        # Extract entities from text
-        entities = extract_entities(text)
-        # Display entities
-        st.subheader("Entities:")
-        for entity in entities:
-            st.write(entity)
-
-# Run app
-if __name__ == "__main__":
-    main()
-
-
-# In[ ]:
+        st.write(ent.text, ent.label_)
 
 
 
