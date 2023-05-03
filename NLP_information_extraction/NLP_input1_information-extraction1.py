47f888177fce327e7dcb4ebfa1b369017317880e
Rahul <116004410+Rahuldahiya67@users.noreply.github.com>
2023-05-03 18:26:26 +0530

    Update NLP_input1_information-extraction1.py

--- NLP_information_extraction/NLP_input1_information-extraction1.py
+++ NLP_information_extraction/NLP_input1_information-extraction1.py
@@ -40,4 +40,22 @@ def app():
     st.markdown(f'<style>{main_css}</style>', unsafe_allow_html=True)
     
     # Add a title
-    st.markdown('<h1 class="title" style="{}
+    st.markdown('<h1 class="title" style="{}">PDF Entity Extraction</h1>'.format(title_css), unsafe_allow_html=True)
+    
+    # Add an upload button
+    st.markdown('<div class="upload" style="{}"><p>Upload a PDF file to extract entities:</p><br><br><br><br></div>'.format(upload_css), unsafe_allow_html=True)
+    
+    # Set PDF file path
+    pdf_file = st.file_uploader(" ", type="pdf")
+
+    # If PDF file is uploaded
+    if pdf_file is not None:
+        # Extract text from PDF file
+        text = extract_text(pdf_file)
+        
+        # Create a spaCy object from the text
+        doc = nlp(text)
+
+        # Display entities in Streamlit
+        for ent in doc.ents:
+            st.write(ent.text, ent.label_)
