666de03157c55221ce72342b66fa991536b35dfa
Rahul <116004410+Rahuldahiya67@users.noreply.github.com>
2023-05-03 18:56:55 +0530

    Update NLP_input1_information-extraction1.py

--- NLP_information_extraction/NLP_input1_information-extraction1.py
+++ NLP_information_extraction/NLP_input1_information-extraction1.py
@@ -1,8 +1,11 @@
-import os
+#!/usr/bin/env python
+# coding: utf-8
+
 import spacy
+from spacy.cli import download
 import streamlit as st
 from pdfminer.high_level import extract_text
-from spacy.cli import download
+import os
 
 # Download spaCy model if not installed
 if not spacy.util.is_package("en_core_web_sm"):
@@ -14,48 +17,17 @@ os.environ["SPACY_DATA"] = "NLP_information_extraction/model.pkl" # Replace with
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Set up some CSS styles
-main_css = """
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 1rem;
-"""
+# Set PDF file path
+pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
 
-title_css = """
-    text-align: center;
-    font-size: 2.5rem;
-    margin-bottom: 2rem;
-"""
-
-upload_css = """
-    padding: 1rem;
-    border: 2px dashed #ccc;
-    text-align: center;
-"""
-
-# Define the Streamlit app
-def app():
-    # Set the page title and add some CSS styles
-    st.set_page_config(page_title="PDF Entity Extraction", page_icon="📚")
-    st.markdown(f'<style>{main_css}</style>', unsafe_allow_html=True)
+# If PDF file is uploaded
+if pdf_file is not None:
+    # Extract text from PDF file
+    text = extract_text(pdf_file)
     
-    # Add a title
-    st.markdown('<h1 class="title" style="{}">PDF Entity Extraction</h1>'.format(title_css), unsafe_allow_html=True)
-    
-    # Add an upload button
-    st.markdown('<div class="upload" style="{}"><p>Upload a PDF file to extract entities:</p><br><br><br><br></div>'.format(upload_css), unsafe_allow_html=True)
-    
-    # Set PDF file path
-    pdf_file = st.file_uploader(" ", type="pdf")
+    # Create a spaCy object from the text
+    doc = nlp(text)
 
-    # If PDF file is uploaded
-    if pdf_file is not None:
-        # Extract text from PDF file
-        text = extract_text(pdf_file)
-        
-        # Create a spaCy object from the text
-        doc = nlp(text)
-
-        # Display entities in Streamlit
-        for ent in doc.ents:
-            st.write(ent.text, ent.label_)
+    # Display entities in Streamlit
+    for ent in doc.ents:
+        st.write(ent.text, ent.label_)
