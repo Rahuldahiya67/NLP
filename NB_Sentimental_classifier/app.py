66c95c12cd307e630f291758536f41bf052bcd37
Rahul <116004410+Rahuldahiya67@users.noreply.github.com>
2023-05-05 12:39:01 +0530

    Update app.py

--- NB_Sentimental_classifier/app.py
+++ NB_Sentimental_classifier/app.py
@@ -1,79 +1,55 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import pickle
+import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-import streamlit as st
-import pickle
 
-# Define the Streamlit app
-st.set_page_config(page_title="classifier", page_icon=":newspaper:", layout="wide")
-st.title("classifier")
+# Load the dataset
+review = pd.read_csv('reviews.csv')
+review = review.rename(columns={'text': 'review'})
 
-# Define the trainmodel function
+# Split the dataset into training and testing sets
+from sklearn.model_selection import train_test_split
+
+X = review.review
+y = review.polarity
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=1)
+
+# Define the trainmodel function to train the model and save it to a file
 def trainmodel():
-    #load data
-    review = pd.read_csv('reviews.csv')
-    review = bbc_text.rename(columns = {'text': 'review'}, inplace = False)
-    
-    #split data
-    X_train, X_test, y_train, y_test = train_test_split(review.review, review.polarity, train_size = 0.6, random_state = 1)
-    
-    #fit vectorizer on training data
+    # Train the vectorizer and classifier
     vector = CountVectorizer(stop_words='english', lowercase=False)
     vector.fit(X_train)
-    
-    #transform data for training and testing
     X_train_transformed = vector.transform(X_train)
-    X_test_transformed = vector.transform(X_test)
-    
-    #train naive bayes classifier
     naivebayes = MultinomialNB()
     naivebayes.fit(X_train_transformed, y_train)
-    
-    #evaluate classifier
-    accuracy = naivebayes.score(X_test_transformed, y_test)
-    st.write('Accuracy:', accuracy)
-    
-    #save model
-    with open('saved_model.pkl', 'wb') as file:
-        pickle.dump(naivebayes, file)
 
-# Define the predict function
+    # Save the model to a file
+    with open('saved_model.pkl', 'wb') as f:
+        pickle.dump((vector, naivebayes), f)
+
+# Define the predict function to take user input and predict the sentiment
 def predict():
-    #load saved model
-    with open('saved_model.pkl', 'rb') as file:
-        saved_model = pickle.load(file)
-    
-    #get user input
-    user_input = st.text_input('Enter a review:')
-    
-    #make prediction
-    if user_input:
-        review = [user_input]
-        vector = CountVectorizer(stop_words='english', lowercase=False)
-        with open('vector.pkl', 'wb') as file:
-            pickle.dump(vector, file)
-        vector.fit(review)
-        vec = vector.transform(review).toarray()
-        prediction = saved_model.predict(vec)[0]
-        if prediction == 1:
-            st.write('The review is positive.')
+    # Load the saved model from the file
+    with open('saved_model.pkl', 'rb') as f:
+        vector, naivebayes = pickle.load(f)
+
+    # Get the user input
+    input_text = st.text_input('Enter your review')
+
+    # Make a prediction
+    if st.button('Predict'):
+        vec = vector.transform([input_text]).toarray()
+        prediction = naivebayes.predict(vec)[0]
+        if prediction == 0:
+            st.write('Negative')
         else:
-            st.write('The review is negative.')
+            st.write('Positive')
 
-# Define the Streamlit app
-def main():
-    st.sidebar.title("Navigation")
-    app_mode = st.sidebar.selectbox("Choose the app mode", ["Homepage", "Train Model", "Make Prediction"])
-    
-    if app_mode == "Homepage":
-        st.write("Welcome to the classifier app!")
-    
-    elif app_mode == "Train Model":
-        trainmodel()
-    
-    elif app_mode == "Make Prediction":
-        predict()
+# Call the trainmodel function to train and save the model
+trainmodel()
 
-if __name__ == "__main__":
-    main()
+# Create the Streamlit app
+st.set_page_config(page_title='Sentiment Analysis', page_icon=':smiley:')
+st.title('Sentiment Analysis')
+predict()
